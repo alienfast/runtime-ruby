497c746481811715a078905ff4ffe90b21b654cd
Kevin Ross <kevin.ross@alienfast.com>
2018-05-14 12:28:21 -0500

    oops - wrong name

--- shell.sh
+++ shell.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it -v $(pwd):/home/circleci/runtime-ruby alienfast/ci-ruby:latest /bin/bash
+docker run -it -v $(pwd):/home/circleci/runtime-ruby alienfast/runtime-ruby:latest /bin/bash
